573cbae046887b5ca91f5ebb3c1a254a420084a3
power.jg <power.jg@d8556058-2737-0410-bf1a-4129cb2bfb7e>
2007-08-08 03:03:34 +0000

    Added all current code that I have.

---
+++ trunk/proj.py
@@ -0,0 +1,118 @@
+import objects
+import BioPythonGUI
+
+class Project:
+
+    def __init__(self, name='New proj'):
+	
+	self.projectName = ''
+	self.items = {}
+	self.changed = False
+	self.filename = ''
+	self.base = None
+	self.currentItem = None
+
+    def addItem(self, item):
+	
+	self.items[item.name] = item
+	self.changed = True
+	try:
+	    self.base.treestoreUpdate()
+	except:
+	    pass
+
+    def getItem(self, item):
+	
+	if type(item)==str:
+	    return self.items[item]
+	else:
+	    return self.items[item.name]
+	self.changed = True
+
+    def delItem(self, item):
+
+	if type(item)==str:
+	    del items[item]
+	else:
+	    del items[item.name]
+	self.changed = True
+	self.base.treestoreUpdate()
+
+    def getDBItems(self):
+	
+	out = []
+	for item in self.items.values():
+	    if item.__class__ == objects.DBItem:
+		out.append(item)
+	
+	return out
+
+    def getDBItemNames(self):
+	
+	out = []
+	for item in self.items.values():
+	    if item.__class__ == objects.DBItem:
+		out.append(item.name)
+
+	return out
+
+    def getSequenceItems(self):
+
+	out = []
+	for item in self.items.values():
+	    if item.__class__ == objects.SequenceItem:
+		out.append(item)
+
+	return out
+
+    def getSequenceItemNames(self):
+
+	out = []
+	for item in self.items.values():
+	    if item.__class__ == objects.SequenceItem:
+		out.append(item.name)
+
+	return out
+
+    def getDBQueries(self):
+
+	out = []
+	for item in self.items.values():
+	    if item.__class__ == objects.DBQuery:
+		out.append(item)
+
+	return out
+
+    def getDBQueryNames(self):
+
+	out = []
+	for item in self.items.values():
+	    if item.__class__ == objects.DBQuery:
+		out.append(item.name)
+
+	return out
+
+    def getNames(self):
+	return self.items.keys()
+
+    
+    def save(self, filename=0):
+	
+	if filename==0:
+	    filename=self.filename
+	
+	import cPickle
+
+	ext = filename.split('.')[-1]
+	if ext == filename:
+	    filename += '.gbp'
+
+	self.filename = filename
+
+	file = open(filename, 'wb')
+
+	cPickle.dump(self, file, -1)
+
+	file.close()
+
+	self.changed = False
---
+++ trunk/readme.txt
@@ -0,0 +1,11 @@
+Requires
+	Gtk+
+	pygtk
+	biopython
+
+These are available from http://www.gtk.org and http://www.pygtk.org
+and http://biopython.org
+(I think)
+
+To run:
+	run python BioPythonGUI
